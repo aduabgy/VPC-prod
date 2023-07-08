0a61cfb91779249a02aae1fc9dd495deb8642029
aduabgy <aduabgy@yahoo.com>
2023-07-08 16:59:18 +0100

    complete vpc for future production

---
+++ vpc.tf
@@ -0,0 +1,165 @@
+/*provider "aws" {
+  region = "${var.region}"
+} 
+
+terraform {
+  backend "s3" {}  
+} */
+
+resource "aws_vpc" "production_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "production_VPC"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1" {
+  cidr_block        = var.public_subnet_1_cidr
+  vpc_id            = aws_vpc.production_vpc.id
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "Public-Subnet-1"
+  }
+}
+
+
+resource "aws_subnet" "public-subnet-2" {
+  cidr_block        = var.public_subnet_2_cidr
+  vpc_id            = aws_vpc.production_vpc.id
+  availability_zone = "eu-west-2b"
+
+  tags = {
+    Name = "Public-Subnet-2"
+  }
+}
+
+resource "aws_subnet" "public-subnet-3" {
+  cidr_block        = var.public_subnet_3_cidr
+  vpc_id            = aws_vpc.production_vpc.id
+  availability_zone = "eu-west-2c"
+
+  tags = {
+    Name = "Public-Subnet-3"
+  }
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  cidr_block        = var.private_subnet_1_cidr
+  vpc_id            = aws_vpc.production_vpc.id
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "Private-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  cidr_block        = var.private_subnet_2_cidr
+  vpc_id            = aws_vpc.production_vpc.id
+  availability_zone = "eu-west-2b"
+
+  tags = {
+    Name = "Private-Subnet-2"
+  }
+}
+
+resource "aws_subnet" "private-subnet-3" {
+  cidr_block        = var.private_subnet_3_cidr
+  vpc_id            = aws_vpc.production_vpc.id
+  availability_zone = "eu-west-2c"
+
+  tags = {
+    Name = "Private-Subnet-3"
+  }
+}
+
+# RouteTables
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.production_vpc.id
+  tags = {
+    Name = "public_route_table"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.production_vpc.id
+  tags = {
+    Name = "private_route_table"
+  }
+}
+
+
+
+resource "aws_route_table_association" "public-subnet-1-association" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
+
+resource "aws_route_table_association" "public-subnet-2-association" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
+
+resource "aws_route_table_association" "public-subnet-3-association" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public-subnet-3.id
+}
+
+resource "aws_route_table_association" "private-subnet-1-association" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+}
+
+resource "aws_route_table_association" "private-subnet-2-association" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private-subnet-2.id
+}
+
+resource "aws_route_table_association" "private-subnet-3-association" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private-subnet-3.id
+}
+
+
+resource "aws_eip" "elastic-ip-for-nat-gw" {
+  domain                    = "vpc"
+  associate_with_private_ip = "10.0.0.5"
+
+  tags = {
+    Name = "Production EIP"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.elastic-ip-for-nat-gw.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+
+  tags = {
+    Name = "Production-Nat-GW"
+  }
+
+  depends_on = [aws_eip.elastic-ip-for-nat-gw]
+}
+
+resource "aws_route" "nat-gw-route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  nat_gateway_id         = aws_nat_gateway.nat-gw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_internet_gateway" "production-IGW" {
+  vpc_id = aws_vpc.production_vpc.id
+
+  tags = {
+    Name = "Production-IGW"
+  }
+}
+
+resource "aws_route" "public-internet-gw-route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  gateway_id             = aws_internet_gateway.production-IGW.id
+  destination_cidr_block = "0.0.0.0/0"
+}    
